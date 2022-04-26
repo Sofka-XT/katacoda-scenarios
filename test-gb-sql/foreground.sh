62ca634696ad3ae8a196c755df1d98e136739f70
raalzate <alzategomez.raul@gmail.com>
2022-04-26 09:31:05 -0500

    ADD test gb sql

--- test-gb-sql/foreground.sh
+++ test-gb-sql/foreground.sh
@@ -1,3 +1,7 @@
+sudo apt update
 sudo apt install mysql-server -y
 sudo apt install mysql-client -y
 sudo service mysql start
+sudo service mysql status
+sudo ss -tap | grep mysql
+/usr/bin/mysql -u root -p
